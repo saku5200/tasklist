07a6d7b524cd763f02dffb66218fbe4d08ea3d78
sou.tanikawa <tanisou0520@gmail.com>
2021-09-09 11:53:48 +0900

    大枠のtasklist完成

--- src/controllers/New.java
+++ src/controllers/New.java
@@ -1,9 +1,8 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-import utils.DBUtil;
 
 /**
  * Servlet implementation class New
@@ -32,26 +30,15 @@ public class New extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
-            EntityManager em = DBUtil.createEntityManager();
-            em.getTransaction().begin();
+     // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
 
-            Task m = new Task();
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("task", new Task());
 
-            String content = request.getParameter("content");
-            m.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setCreated_at(currentTime);
-            m.setUpdated_at(currentTime);
-
-            em.persist(m);
-            em.getTransaction().commit();
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        rd.forward(request, response);
         }
     }
 
-}
+
